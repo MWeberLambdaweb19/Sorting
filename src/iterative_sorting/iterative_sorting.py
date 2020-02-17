58eaaf156cef31220e3bf1cc74bba4296a349ebb
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-02-17 13:13:03 -0800

    Initial commit

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,22 +1,34 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
+    if len(arr) == 0:
+        return arr
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+
+        print('cur', cur_index)
+        print('sml', smallest_index)
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        # (hint, can do in 3 loc)
+        arr_len = len(arr)
+        while arr_len > 0:
+            if arr[smallest_index] >= arr[cur_index]:
+                smallest_index = cur_index
+                cur_index += 1
+            else:
+                arr[0] = arr[smallest_index] 
+
 
 
 
         # TO-DO: swap
 
-
-
-
     return arr
 
+test_array = [5, 4, 1, 3, 6, 2]
+
+selection_sort(test_array)
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
@@ -28,3 +40,20 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+
+# Legacy code 
+
+    # Problem 1
+
+        # TO-DO: find next smallest element
+        # (hint, can do in 3 loc) 
+        # if arr[smallest_index] >= arr[cur_index+1]:
+        #     cur_index = i+1
+        #     print(arr[smallest_index])
+        # elif arr[smallest_index] <= arr[cur_index+1]:
+        #     smallest_index = i+1
+        #     print(arr[smallest_index])
+        # else:
+        #     arr[0] = smallest_index
+              
