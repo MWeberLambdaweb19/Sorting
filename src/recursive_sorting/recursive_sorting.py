081a19a7cb610f23f24dced64081da0e93b019c1
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-02-18 08:39:51 -0800

    Initial commit for the day

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,6 +3,14 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
+    for i in elements:
+        # if all elements of len arrA are in elements, put in elements
+        # of len arrB
+        # if all elements of len B are in elements, put in elements
+        # of len arrA
+        # if next element in arrA is smaller, add to merged array
+        # if next element in arrB is smaller, add to merged array
+
     
     return merged_arr
 
@@ -10,6 +18,9 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    # need to split this element in half
+    # need to split those elements in half down to one using recursion
+    
 
     return arr
 
